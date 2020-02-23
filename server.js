b839f99d2affd1093661eef5a3be80bc409475f0
chetan <cchetantayade@gmail.com>
2020-02-24 00:57:23 +0530

    pay

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const port =  3000;
 const app = express();
 
-
+const payumoney = require('payumoney-node');
+payumsoney.setKeys(MERCHANT_KEY, MERCHANT_SALT, AUTHORIZATION_HEADER);
 
 
 app.listen(port,()=>{
